cc726c966c7e9a88fd7355c7535dd0f6787d1b75
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2021-04-13 16:45:50 -0300

    Adding 'Box' component.

---
+++ src/components/Box.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeaderStart from './HeaderStart'
+import './Box.css'
+
+function Boxes() {
+    return (
+        <div className='Bigbox'>
+            <HeaderStart nome='João Gabriel de Campos de Matos' />
+
+            <div className='Box'>
+                <p className='p1'>Este é meu curriculo.</p>
+                <p className='p2'>Clique em visualizar para me conhecer um pouco melhor.</p>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default Boxes
